42a274e18ae3cacb302775832bf20aabda79621e
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-30 11:05:55 +0000

    Connect to React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
 
 function App() {
   return (
     <div className="App">
       <h1>This is a recipe app</h1>
+      <ButtonGroup aria-label="Basic example">
+        <Button variant="secondary">Left</Button>
+        <Button variant="secondary">Middle</Button>
+        <Button variant="secondary">Right</Button>
+      </ButtonGroup>
     </div>
   );
 }
