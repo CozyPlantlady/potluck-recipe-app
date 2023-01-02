a9c80547a738ef0af7dd2ebf8c94490e62f2b220
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-02 09:28:04 +0000

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1>This is a recipe app</h1>
-      <ButtonGroup aria-label="Basic example">
-        <Button variant="secondary">Left</Button>
-        <Button variant="secondary">Middle</Button>
-        <Button variant="secondary">Right</Button>
-      </ButtonGroup>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap'
+
+const NavBar = () => {
+  return (
+    <Navbar bg="info" variant="dark" expand="lg" fixed="top">
+        <Container>
+            <Navbar.Brand varinat="white" href="#home">Potlucky</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#profile">Profile</Nav.Link>
+                </Nav>
+                <Form inline>
+                <FormControl type="text" placeholder="find a recipe" className="mr-sm-2" />
+                <Button variant="light">Search</Button>
+                </Form>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar
