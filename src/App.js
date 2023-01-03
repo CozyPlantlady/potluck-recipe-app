379fe8184b405722d390670c11bd7a0a93725d68
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-03 09:12:56 +0000

    Adjusting navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.module.css';
 import NavBar from './components/NavBar';
 
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,16 +9,16 @@ const NavBar = () => {
             <Navbar.Brand variant="white" href="#home"><img src={logo} alt="logo" height="60" /> Potlucky</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#profile">Profile</Nav.Link>
+                <Nav className="ml-auto">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#profile">Profile</Nav.Link>
+                  <Form>
+                    <FormControl type="text" placeholder="find a recipe"/>
+                    <Button variant="light">Search</Button>
+                  </Form>
                 </Nav>
-                <Form inline>
-                <FormControl type="text" placeholder="find a recipe" className="mr-sm-2" />
-                <Button variant="light">Search</Button>
-                </Form>
             </Navbar.Collapse>
-        </Container>
+          </Container>
     </Navbar>
   )
 }
