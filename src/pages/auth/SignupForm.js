8167a6892ac553452c9eb81620c400dcad04eae0
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-03 18:34:02 +0000

    Remove unused boiler code and fix colors

--- src/pages/auth/SignupForm.js
+++ src/pages/auth/SignupForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from '/workspace/potluck-recipe-app/src/assets/logo.png'
+import styles from '/workspace/potluck-recipe-app/src/components/styles/signUpForm.module.css'
 
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
 
@@ -8,17 +9,37 @@ const SignUpForm = () => {
     return (
       <Row>
         <Col className="my-auto py-2 p-md-2" md={6}>
+            <Container className={`${styles.link} mt-3`}>
+                <Link to="/signin">
+                Already have an account? <span>Sign in</span>
+                </Link>
+            </Container>
           <Container className="p-4">
-            <h1>sign up</h1>
+            
+            <h1>Sign up</h1>
   
-              {/* add your form here */}
-  
+            <Form>
+                <Form.Group controlId="username">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder="Enter username" name="username" />
+                </Form.Group>
+
+                <Form.Group controlId="password1">
+                    <Form.Label className="d-none">Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" name="password1" />
+                </Form.Group>
+
+                <Form.Group controlId="password2">
+                    <Form.Label className="d-none">Repeat password</Form.Label>
+                    <Form.Control type="password" placeholder="Repeat password" name="password2" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
           </Container>
-          <Container className="mt-3">
-            <Link to="/signin">
-              Already have an account? <span>Sign in</span>
-            </Link>
-          </Container>
+
+          
         </Col>
         <Col
           md={6}
