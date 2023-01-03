3c0f2ff3883f73f086c3f6f5d54c7df768d35de9
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-03 10:06:11 +0000

    Add router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from "react-bootstrap/Container"
+import {Route,Switch} from 'react-router-dom'
 
 
 function App() {
@@ -9,6 +10,10 @@ function App() {
       <NavBar></NavBar>
       <Container className={styles.Main}>
         <h1>This is a recipe app</h1>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/profile" render={() => <h1>Profile</h1>} />
+        </Switch>
       </Container>
     </div>
   );
