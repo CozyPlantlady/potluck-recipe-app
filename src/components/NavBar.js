64a32673d5d62f986db7cdbd24c8d747a0fce89c
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-03 10:14:01 +0000

    Add NavLinks to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,24 @@ import React from 'react'
 import { Container, Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap'
 import logo from '../assets/logo.png'
 import styles from '../components/styles/NavBar.module.css'
+import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <Navbar bg="info" variant="dark" expand="lg" fixed="top">
+    <Navbar bg="info" variant="dark" expand="lg" fixed="top" className={styles.navbartext}>
         <Container>
-            <Navbar.Brand className={styles.logo} variant="white" href="#home"><img src={logo} alt="logo" height="60" /> Potlucky</Navbar.Brand>
+          <NavLink to="/">
+            <Navbar.Brand 
+            className={styles.logo} 
+            variant="white">
+              <img src={logo} alt="logo" height="60" />Potlucky
+            </Navbar.Brand>
+          </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Collapse id="basic-navbar-nav" >
                 <Nav className="ml-auto">
-                  <Nav.Link className={styles.navbartext} href="#home">Home</Nav.Link>
-                  <Nav.Link className={styles.navbartext} href="#profile">Profile</Nav.Link>
+                  <NavLink to="/">Home</NavLink>
+                  <NavLink to="/profile">Profile</NavLink>
                   <Form>
                     <FormControl type="text" placeholder="find a recipe"/>
                     <Button variant="light">Search</Button>
