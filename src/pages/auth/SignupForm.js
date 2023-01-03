35346be3a19bd9452b0d1019eacc8c40691d4da9
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-03 20:22:17 +0000

    Add functionality to SignupForm

--- src/pages/auth/SignupForm.js
+++ src/pages/auth/SignupForm.js
@@ -1,11 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useHistory } from "react-router-dom";
 import logo from '../../assets/logo.png';
 import styles from '../../components/styles/signUpForm.module.css';
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+import axios from "axios";
 
 const SignUpForm = () => {
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        password1: "",
+        password2: "",
+    });
+
+    const { username, password1, password2 } = signUpData;
+
+    const [errors, setErrors] = useState({});
+
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('/dj-rest-auth/registration/', signUpData)
+            history.push('/signin')
+        } catch(err){
+            setErrors(err.response?.data)
+        }
+    };
+
     return (
       <Row>
         <Col className="my-auto py-2 p-md-2" md={6}>
@@ -18,24 +48,56 @@ const SignUpForm = () => {
             
             <h1>Sign up</h1>
   
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="username">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Enter username" name="username" />
+                    <Form.Label className="d-none">Username</Form.Label>
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Enter username" 
+                        name="username"
+                        value={username}
+                        onChange={handleChange}
+                    />
                 </Form.Group>
+                {errors.username?.map((message, idx) =>
+                    <Alert variant="warning" key={idx}>{message}</Alert>
+                )}
 
                 <Form.Group controlId="password1">
                     <Form.Label className="d-none">Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" name="password1" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        name="password1"
+                        value={password1}
+                        onChange={handleChange}
+                    />
                 </Form.Group>
+                {errors.password1?.map((message, idx) =>
+                    <Alert variant="warning" key={idx}>{message}</Alert>
+                )}
 
                 <Form.Group controlId="password2">
                     <Form.Label className="d-none">Repeat password</Form.Label>
-                    <Form.Control type="password" placeholder="Repeat password" name="password2" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Repeat password"
+                        name="password2"
+                        value={password2}
+                        onChange={handleChange}
+                    />
                 </Form.Group>
+                {errors.password2?.map((message, idx) =>
+                    <Alert variant="warning" key={idx}>{message}</Alert>
+                )}
+
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
+                {errors.non_field_errors?.map((message, idx) =>
+                    <Alert variant="warning" key={idx}>{message}</Alert>
+                )}
+
             </Form>
           </Container>
 
