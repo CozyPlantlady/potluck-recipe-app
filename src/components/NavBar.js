e7d2fac77a89d0030f7d622b7e203e1658d39adb
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-05 17:37:31 +0000

    Add logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,47 @@ import { Container, Nav, Navbar, Form, FormControl, Button } from 'react-bootstr
 import logo from '../assets/logo.png'
 import styles from '../components/styles/NavBar.module.css'
 import { NavLink } from "react-router-dom"
-import { useCurrentUser } from '../contexts/CurrentUserContext'
+import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext'
+import axios from 'axios'
 
 const NavBar = () => {
   const currentUser = useCurrentUser;
+  const setCurrentUser = useSetCurrentUser;
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const handleLogOut = async () => {
+    try {
+        await axios.post("dj-rest-auth/logout/");
+        setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addRecipe = (
+    <NavLink 
+    className={styles.NavLink}
+    to="/recipe/create"
+    >Add recipe
+  </NavLink>
+  );
+
+  const loggedInIcons = 
+  <>
+    <NavLink 
+      className={styles.NavLink}
+      to="/"
+      onClick={handleLogOut}>
+        Log out
+    </NavLink>
+
+    <NavLink 
+      className={styles.NavLink}
+      to={`/profiles/${currentUser?.profile_id}`}
+      >
+      <img src={currentUser?.profile_image} alt="profile"/>
+    </NavLink>
+  </>;
+
   const loggedOutIcons = (
     <>
     <p> You are currently logged out</p>
@@ -16,8 +51,8 @@ const NavBar = () => {
       to="/signup"
       className={styles.NavLink}
       >Sign up
-      </NavLink>
-      <NavLink 
+    </NavLink>
+    <NavLink 
       to="/login"
       className={styles.NavLink}
       >Login
@@ -35,6 +70,7 @@ const NavBar = () => {
               <img src={logo} alt="logo" height="100" />Potlucky
             </Navbar.Brand>
           </NavLink>
+          {currentUser && addRecipe}
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" >
                   <Form>
