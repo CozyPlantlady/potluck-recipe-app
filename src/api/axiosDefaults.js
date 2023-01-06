00e304d2a0440b4da90d3df8a45fefbdc2d1ef30
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-06 18:19:37 +0000

    Change axios baseURL back to herokuapp

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://8000-cozyplantla-drfapipotlu-7sgubgy7qdk.ws-eu81.gitpod.io/';
+axios.defaults.baseURL = 'https://cozy-potlucky.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
