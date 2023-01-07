52937c07311d62a10c11464078a7f1154e970da2
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-07 14:23:39 +0000

    Add RecipeCreateForm

--- src/pages/recipes/RecipeCreateForm.js
+++ src/pages/recipes/RecipeCreateForm.js
@@ -6,18 +6,81 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Asset from "../../components/Asset";
+import { Image } from "react-bootstrap";
 
 function RecipeCreateForm() {
 
+    const [recipeData, setRecipeData] = useState({
+        title: "",
+        recipe: "",
+        image: "",
+        method: "",
+        keywords: "",
+      });
+
+    const { title, recipe, image, method, keywords } = recipeData;
+
     const [errors, setErrors] = useState({});
-  
+
+    const handleChange = (event) => {
+        setRecipeData({
+            ...recipeData,
+            [event.target.name]:event.target.value,
+        });
+    };
+
+    const handleChangeImage = (event) => {
+        if (event.target.files.length){
+            URL.revokeObjectURL(image);
+            setRecipeData({
+                ...recipeData,
+                image: URL.createObjectURL(event.target.files[0])
+            });
+        }
+    };
   
     const textFields = (
       <div className="text-center">
-        {/* Add your form fields here */}
-  
-      
-      
+        <Form.Group>
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+                type="text"
+                name="title"
+                value={title}
+                onChange={handleChange}
+            />
+        </Form.Group>
+
+        <Form.Group>
+            <Form.Label>Recipe</Form.Label>
+            <Form.Control
+                type="textarea"
+                name="recipe"
+                value={recipe}
+                onChange={handleChange}
+            />
+        </Form.Group>
+
+        <Form.Group>
+            <Form.Label>Method</Form.Label>
+            <Form.Control
+                type="text"
+                name="method"
+                value={method}
+                onChange={handleChange}
+            />
+        </Form.Group>
+
+        <Form.Group>
+            <Form.Label>Keywords</Form.Label>
+            <Form.Control
+                type="textarea"
+                name="keywords"
+                value={keywords}
+                onChange={handleChange}
+            />
+        </Form.Group>
+
         <Button
           onClick={() => {}}
         >
@@ -32,22 +95,42 @@ function RecipeCreateForm() {
     return (
         <Form>
           <Row>
+            
+            <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+              <Container>{textFields}</Container>
+            </Col>
             <Col>
               <Container>
                 <Form.Group className="text-center">
-                    <Form.Label
+                    {image ? (
+                        <>
+                            <figure>
+                                <Image src={image}/>
+                            </figure>
+                            <div>
+                                <Form.Label 
+                                    className="btn"
+                                    >
+                                    Change to another picture
+                                </Form.Label> 
+                            </div>
+                        </>
+                    ) : (
+                        <Form.Label
                       className="d-flex justify-content-center"
-                      htmlFor="image-upload"
-                    >
-                      <Asset message="Click to add a picture of the dish" />
+                      htmlFor="image-upload">
+                        <Asset message="Click to add a picture of the dish" />
                     </Form.Label>
+                    )}
+                    
+                    <Form.File
+                        id="image-upload"
+                        accept="image/*"
+                        onChange={handleChangeImage}/>
     
                 </Form.Group>
                 <div className="d-md-none">{textFields}</div>
               </Container>
-            </Col>
-            <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
-              <Container>{textFields}</Container>
             </Col>
           </Row>
         </Form>
