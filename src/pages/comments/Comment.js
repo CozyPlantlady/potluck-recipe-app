92c0a43be4991cecf8a8a6b724016db564710770
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-29 15:16:11 +0000

    Allow user to edit comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Media } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { axiosRes } from '../../api/axiosDefaults';
 import { DropdownEditDelete } from '../../components/DropdownEditDelete';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
-import styles from "../../styles/Comment.module.css"
+import styles from "../../styles/Comment.module.css";
+import CommentEditForm from './CommentEditForm';
+
 
 const Comment = (props) => {
     const {
@@ -16,6 +18,8 @@ const Comment = (props) => {
         id,
         setComments
     } = props;
+
+    const [showEditForm, setShowEditForm] = useState(false);
 
     const currentUser = useCurrentUser()
     const is_owner = currentUser?.username === owner;
@@ -42,17 +46,27 @@ const Comment = (props) => {
             <Media.Body className="align-self-center ml-2">
                 <span className={styles.Owner}>{owner}</span>
                 <span className={styles.Date}>{edited_at}</span>
+                {showEditForm ? (
+                    <CommentEditForm
+                        id={id}
+                        profile_id={profile_id}
+                        content={content}
+                        setComments={setComments}
+                        setShowEditForm={setShowEditForm}
+                    />
+                ) : (
                 <p>{content}</p>
+                )}
             </Media.Body>
             <img
-            alt="comment with a picture of dish"
+            alt="comment of dish"
             height={35}
             width={35}
             className="mr-3"
             src={comment_image}
             />
             {is_owner && (
-                <DropdownEditDelete handleEdit={() => {}} handleDelete={handleDelete} />
+                <DropdownEditDelete handleEdit={() => setShowEditForm(true)} handleDelete={handleDelete} />
             )}
         </Media>
     </div>
